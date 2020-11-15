d6023c8216c42fdcc5e2cc8a827d826a775662a4
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-11-15 18:46:34 +0100

    scrub backtraces in case of failures
    Similar to what Test does. This entailed to make a new
    ReTestSet <: AbstractTestSet type, and to copy a lot of
    code from Test. But it was likely code would have had to
    be copied anyway for implementing other features.

--- src/testset.jl
+++ src/testset.jl
@@ -1,11 +1,288 @@
 module Testset
 
-using Test: DefaultTestSet, Error, Test, _check_testset, finish, get_testset,
-            get_testset_depth, parse_testset_args, pop_testset, push_testset, record
+using Test: AbstractTestSet, Broken, DefaultTestSet, Error, Fail, Pass, Test,
+            TestSetException, get_testset, get_testset_depth,
+            parse_testset_args, pop_testset, push_testset
+
+import Test: finish, record
 
 import Random
 
 import InlineTest: @testset
+
+# mostly copied from Test stdlib
+# changed from Test: pass nothing as file in ip_has_file_and_func
+#-----------------------------------------------------------------------
+
+# (this has to be copied from Test, because `@__FILE__` is hardcoded)
+
+# Backtrace utility functions
+function ip_has_file_and_func(ip, file, funcs)
+    return any(fr -> ((file === nothing || string(fr.file) == file) &&
+                      fr.func in funcs),
+               StackTraces.lookup(ip))
+end
+
+function scrub_backtrace(bt)
+    do_test_ind = findfirst(ip -> ip_has_file_and_func(ip, nothing,
+                                                       (:do_test, :do_test_throws)), bt)
+    if do_test_ind !== nothing && length(bt) > do_test_ind
+        bt = bt[do_test_ind + 1:end]
+    end
+    name_ind = findfirst(ip -> ip_has_file_and_func(ip, @__FILE__, (Symbol("macro expansion"),)), bt)
+    if name_ind !== nothing && length(bt) != 0
+        bt = bt[1:name_ind]
+    end
+    return bt
+end
+
+function scrub_exc_stack(stack)
+    return Any[ (x[1], scrub_backtrace(x[2])) for x in stack ]
+end
+
+mutable struct ReTestSet <: AbstractTestSet
+    description::AbstractString
+    results::Vector
+    n_passed::Int
+    anynonpass::Bool
+    verbose::Bool
+end
+ReTestSet(desc; verbose = false) = ReTestSet(desc, [], 0, false, verbose)
+
+# For a broken result, simply store the result
+record(ts::ReTestSet, t::Broken) = (push!(ts.results, t); t)
+# For a passed result, do not store the result since it uses a lot of memory
+record(ts::ReTestSet, t::Pass) = (ts.n_passed += 1; t)
+
+# For the other result types, immediately print the error message
+# but do not terminate. Print a backtrace.
+function record(ts::ReTestSet, t::Union{Fail, Error})
+    printstyled(ts.description, ": ", color=:white)
+    # don't print for interrupted tests
+    if !(t isa Error) || t.test_type !== :test_interrupted
+        print(t)
+        if !isa(t, Error) # if not gets printed in the show method
+            Base.show_backtrace(stdout, scrub_backtrace(backtrace()))
+        end
+        println()
+    end
+
+    push!(ts.results, t)
+    return t
+end
+
+# When a ReTestSet finishes, it records itself to its parent
+# testset, if there is one. This allows for recursive printing of
+# the results at the end of the tests
+record(ts::ReTestSet, t::AbstractTestSet) = push!(ts.results, t)
+
+function print_test_errors(ts::ReTestSet)
+    for t in ts.results
+        if isa(t, Error) || isa(t, Fail)
+            println("Error in testset $(ts.description):")
+            show(t)
+            println()
+        elseif isa(t, ReTestSet)
+            print_test_errors(t)
+        end
+    end
+end
+
+function print_test_results(ts::ReTestSet, depth_pad=0)
+    # Calculate the overall number for each type so each of
+    # the test result types are aligned
+    passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken = get_test_counts(ts)
+    total_pass   = passes + c_passes
+    total_fail   = fails  + c_fails
+    total_error  = errors + c_errors
+    total_broken = broken + c_broken
+    dig_pass   = total_pass   > 0 ? ndigits(total_pass)   : 0
+    dig_fail   = total_fail   > 0 ? ndigits(total_fail)   : 0
+    dig_error  = total_error  > 0 ? ndigits(total_error)  : 0
+    dig_broken = total_broken > 0 ? ndigits(total_broken) : 0
+    total = total_pass + total_fail + total_error + total_broken
+    dig_total = total > 0 ? ndigits(total) : 0
+    # For each category, take max of digits and header width if there are
+    # tests of that type
+    pass_width   = dig_pass   > 0 ? max(length("Pass"),   dig_pass)   : 0
+    fail_width   = dig_fail   > 0 ? max(length("Fail"),   dig_fail)   : 0
+    error_width  = dig_error  > 0 ? max(length("Error"),  dig_error)  : 0
+    broken_width = dig_broken > 0 ? max(length("Broken"), dig_broken) : 0
+    total_width  = dig_total  > 0 ? max(length("Total"),  dig_total)  : 0
+    # Calculate the alignment of the test result counts by
+    # recursively walking the tree of test sets
+    align = max(get_alignment(ts, 0), length("Test Summary:"))
+    # Print the outer test set header once
+    pad = total == 0 ? "" : " "
+    printstyled(rpad("Test Summary:", align, " "), " |", pad; bold=true, color=:white)
+    if pass_width > 0
+        printstyled(lpad("Pass", pass_width, " "), "  "; bold=true, color=:green)
+    end
+    if fail_width > 0
+        printstyled(lpad("Fail", fail_width, " "), "  "; bold=true, color=Base.error_color())
+    end
+    if error_width > 0
+        printstyled(lpad("Error", error_width, " "), "  "; bold=true, color=Base.error_color())
+    end
+    if broken_width > 0
+        printstyled(lpad("Broken", broken_width, " "), "  "; bold=true, color=Base.warn_color())
+    end
+    if total_width > 0
+        printstyled(lpad("Total", total_width, " "); bold=true, color=Base.info_color())
+    end
+    println()
+    # Recursively print a summary at every level
+    print_counts(ts, depth_pad, align, pass_width, fail_width, error_width, broken_width, total_width)
+end
+
+# Called at the end of a @testset, behaviour depends on whether
+# this is a child of another testset, or the "root" testset
+function finish(ts::ReTestSet)
+    # If we are a nested test set, do not print a full summary
+    # now - let the parent test set do the printing
+    if get_testset_depth() != 0
+        # Attach this test set to the parent test set
+        parent_ts = get_testset()
+        record(parent_ts, ts)
+        return ts
+    end
+    passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken = get_test_counts(ts)
+    total_pass   = passes + c_passes
+    total_fail   = fails  + c_fails
+    total_error  = errors + c_errors
+    total_broken = broken + c_broken
+    total = total_pass + total_fail + total_error + total_broken
+
+    print_test_results(ts)
+
+    # Finally throw an error as we are the outermost test set
+    if total != total_pass + total_broken
+        # Get all the error/failures and bring them along for the ride
+        efs = filter_errors(ts)
+        throw(TestSetException(total_pass, total_fail, total_error, total_broken, efs))
+    end
+
+    # return the testset so it is returned from the @testset macro
+    ts
+end
+
+# Recursive function that finds the column that the result counts
+# can begin at by taking into account the width of the descriptions
+# and the amount of indentation. If a test set had no failures, and
+# no failures in child test sets, there is no need to include those
+# in calculating the alignment
+function get_alignment(ts::ReTestSet, depth::Int)
+    # The minimum width at this depth is
+    ts_width = 2*depth + length(ts.description)
+    # If all passing, no need to look at children
+    !ts.anynonpass && return ts_width
+    # Return the maximum of this width and the minimum width
+    # for all children (if they exist)
+    isempty(ts.results) && return ts_width
+    child_widths = map(t->get_alignment(t, depth+1), ts.results)
+    return max(ts_width, maximum(child_widths))
+end
+get_alignment(ts, depth::Int) = 0
+
+# Recursive function that fetches backtraces for any and all errors
+# or failures the testset and its children encountered
+function filter_errors(ts::ReTestSet)
+    efs = []
+    for t in ts.results
+        if isa(t, ReTestSet)
+            append!(efs, filter_errors(t))
+        elseif isa(t, Union{Fail, Error})
+            append!(efs, [t])
+        end
+    end
+    efs
+end
+
+# Recursive function that counts the number of test results of each
+# type directly in the testset, and totals across the child testsets
+function get_test_counts(ts::ReTestSet)
+    passes, fails, errors, broken = ts.n_passed, 0, 0, 0
+    c_passes, c_fails, c_errors, c_broken = 0, 0, 0, 0
+    for t in ts.results
+        isa(t, Fail)   && (fails  += 1)
+        isa(t, Error)  && (errors += 1)
+        isa(t, Broken) && (broken += 1)
+        if isa(t, ReTestSet)
+            np, nf, ne, nb, ncp, ncf, nce , ncb = get_test_counts(t)
+            c_passes += np + ncp
+            c_fails  += nf + ncf
+            c_errors += ne + nce
+            c_broken += nb + ncb
+        end
+    end
+    ts.anynonpass = (fails + errors + c_fails + c_errors > 0)
+    return passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken
+end
+
+# Recursive function that prints out the results at each level of
+# the tree of test sets
+function print_counts(ts::ReTestSet, depth, align,
+                      pass_width, fail_width, error_width, broken_width, total_width)
+    # Count results by each type at this level, and recursively
+    # through any child test sets
+    passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken = get_test_counts(ts)
+    subtotal = passes + fails + errors + broken + c_passes + c_fails + c_errors + c_broken
+    # Print test set header, with an alignment that ensures all
+    # the test results appear above each other
+    print(rpad(string("  "^depth, ts.description), align, " "), " | ")
+
+    np = passes + c_passes
+    if np > 0
+        printstyled(lpad(string(np), pass_width, " "), "  ", color=:green)
+    elseif pass_width > 0
+        # No passes at this level, but some at another level
+        print(lpad(" ", pass_width), "  ")
+    end
+
+    nf = fails + c_fails
+    if nf > 0
+        printstyled(lpad(string(nf), fail_width, " "), "  ", color=Base.error_color())
+    elseif fail_width > 0
+        # No fails at this level, but some at another level
+        print(lpad(" ", fail_width), "  ")
+    end
+
+    ne = errors + c_errors
+    if ne > 0
+        printstyled(lpad(string(ne), error_width, " "), "  ", color=Base.error_color())
+    elseif error_width > 0
+        # No errors at this level, but some at another level
+        print(lpad(" ", error_width), "  ")
+    end
+
+    nb = broken + c_broken
+    if nb > 0
+        printstyled(lpad(string(nb), broken_width, " "), "  ", color=Base.warn_color())
+    elseif broken_width > 0
+        # None broken at this level, but some at another level
+        print(lpad(" ", broken_width), "  ")
+    end
+
+    if np == 0 && nf == 0 && ne == 0 && nb == 0
+        printstyled("No tests", color=Base.info_color())
+    else
+        printstyled(lpad(string(subtotal), total_width, " "), color=Base.info_color())
+    end
+    println()
+
+    # Only print results at lower levels if we had failures or if the user
+    # wants.
+    if (np + nb != subtotal) || (ts.verbose)
+        for t in ts.results
+            if isa(t, ReTestSet)
+                print_counts(t, depth + 1, align,
+                    pass_width, fail_width, error_width, broken_width, total_width)
+            end
+        end
+    end
+end
+
+#-----------------------------------------------------------------------
 
 default_rng() = isdefined(Random, :default_rng) ?
     Random.default_rng() :
@@ -31,7 +308,6 @@ end
 Generate the code for a `@testset` with a `begin`/`end` argument
 """
 function testset_beginend(isfinal::Bool, rx::Regex, desc::String, tests, source)
-    testsettype = :(get_testset_depth() == 0 ? DefaultTestSet : typeof(get_testset()))
     # Generate a block of code that initializes a new testset, adds
     # it to the task local storage, evaluates the test(s), before
     # finally removing the testset and giving it a chance to take
@@ -42,9 +318,8 @@ function testset_beginend(isfinal::Bool, rx::Regex, desc::String, tests, source)
             current_str = string(get_testset_string(), '/', $desc)
         end
         if !$isfinal || occursin($rx, current_str)
-            _check_testset($testsettype, $(QuoteNode(testsettype.args[1])))
             local ret
-            local ts = $(testsettype)($desc)
+            local ts = ReTestSet($desc)
             push_testset(ts)
             # we reproduce the logic of guardseed, but this function
             # cannot be used as it changes slightly the semantic of @testset,
@@ -88,16 +363,12 @@ function testset_forloop(isfinal::Bool, rx::Regex, desc::Union{String,Expr},
     # description and we'll definitely need them for generating the
     # comprehension expression at the end
     loopvars = Expr[Expr(:(=), loopiter, loopvals)]
-
-    testsettype = :(get_testset_depth() == 0 ? DefaultTestSet : typeof(get_testset()))
-
     blk = quote
         local current_str
         if $isfinal
             current_str = string(get_testset_string(!first_iteration), '/', $(esc(desc)))
         end
         if !$isfinal || occursin($rx, current_str)
-            _check_testset($testsettype, $(QuoteNode(testsettype.args[1])))
             # Trick to handle `break` and `continue` in the test code before
             # they can be handled properly by `finally` lowering.
             if !first_iteration
@@ -106,7 +377,7 @@ function testset_forloop(isfinal::Bool, rx::Regex, desc::Union{String,Expr},
                 # it's 1000 times faster to copy from tmprng rather than calling Random.seed!
                 copy!(RNG, tmprng)
             end
-            ts = $(testsettype)($(esc(desc)))
+            ts = ReTestSet($(esc(desc)))
             push_testset(ts)
             first_iteration = false
             try
