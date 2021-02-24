3548a37c56cde34f5bb59a07eeacd78f01148c44
Rafael Fourquet <fourquet.rafael@gmail.com>
2021-02-24 19:13:23 +0100

    "loaded modules": find also submodules
    We use `Base.loaded_modules` to find modules using `ReTest` which didn't
    have a chance to register themselves in InlineTest.TESTED_MODULES.
    Here we look also for submodules using ReTest.

--- test/FakePackage/test/runtests.jl
+++ test/FakePackage/test/runtests.jl
@@ -7,3 +7,6 @@ FakePackage.runtests(r"begin-end")
 @test FakePackage.RUN == [1, 2, 1]
 FakePackage.runtests(r" for")
 @test FakePackage.RUN == [1, 2, 1, 2]
+
+retest(dry=true) # only to update InlineTest.TESTED_MODULES
+@test FakePackage.Submodule in ReTest.TESTED_MODULES
